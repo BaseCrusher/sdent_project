db61cdca6fe553f26af71fca8af53f1cd9b7de02
BaseCrusher <ruzinski.jehuda@outlook.com>
2020-10-26 11:01:20 +0100

    added gravitational fields

---
+++ Position2D.gd
@@ -0,0 +1,26 @@
+extends Position2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+onready var character = get_node("../../icon")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _draw():
+	draw_circle(position, 150, Color(0.5,0.5,0.5, 0.7))
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _physics_process(delta):
+	if (global_position - character.position).length() <= 150:
+		character.direction_vector += (global_position - character.position).normalized() * 5
+		character.current_speed += (global_position - character.position).length() / 100 / character.VECTOR_LENGTH_TO_SPEED
+		pass
+	pass
--- icon.gd
+++ icon.gd
@@ -1,18 +1,20 @@
 extends Sprite
 
+# Constants
+const DAMPING : float = 0.97
+const DIRECTION_VECTOR_MULTIPLIER : float = 1.0
+const MAX_SPEED : float = 7.0
+const VECTOR_LENGTH_TO_SPEED : float = 20.0
 
-var is_player_selected : bool = false
+# Variables
+var is_character_selected : bool = false
+# Start position of character before each jump
+var start_position : Vector2 
+# Current speed depends on how much the player pulled the character
+var current_speed : float = 0.0
+# Normalized vector oposite to pulling direction muliplied by DIRECTION_VECTOR_MULTIPLIER
+var direction_vector : Vector2 = Vector2.ZERO
 
-var start_position
-var movement_direction = Vector2.ZERO
-var speed = 0
-
-var direction_vector= Vector2.ZERO
-var manipulation_vector = Vector2(0, 0)
-var velocity = Vector2.ZERO
-
-const SPRING_CONSTANT = 0.4
-const DAMPING = 0.97
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,41 +22,36 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and get_rect().has_point(to_local(event.position)):
+		if event.pressed:
+			on_player_pressed()
+		if not event.pressed:
+			on_player_released()
 
-	pass
 
-
-func _on_Area2D_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
-		on_player_pressed()
-		pass
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and not event.pressed:
-		on_player_released()
-		pass
-	pass # Replace with function body.
-	
 func _physics_process(delta):
-	if is_player_selected:
+	if is_character_selected:
 		global_position = get_global_mouse_position()
 	else:
-		global_position = global_position + ((direction_vector + manipulation_vector ) * speed * delta)
-		speed = speed * DAMPING
-		if speed <=10e-5:
-			speed = 0
+		global_position = global_position + (direction_vector * current_speed * delta)
+		current_speed = current_speed * DAMPING
+		if current_speed <=10e-5:
+			direction_vector = Vector2.ZERO
+			current_speed = 0
 		start_position = global_position
 		pass
 
+
 func on_player_pressed():
-	is_player_selected = true
+	is_character_selected = true
 	pass
 
 func on_player_released():
-	is_player_selected = false
-	direction_vector = -(get_global_mouse_position()-start_position)
-	var direction = direction_vector.normalized()
-	speed = direction_vector.length() / 20
-	if speed > 7:
-		speed = 7
+	is_character_selected = false
+	var temp_direction_vector = -(get_global_mouse_position()-start_position)
+	direction_vector = temp_direction_vector * DIRECTION_VECTOR_MULTIPLIER
+	current_speed = temp_direction_vector.length() / VECTOR_LENGTH_TO_SPEED
+	if current_speed > MAX_SPEED:
+		current_speed = MAX_SPEED
 	pass

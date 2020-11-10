fb35dfb3960b8c92acb3ceba886b3ef1ea0accf5
AndreasRohr <andreas.rohr@students.fhnw.ch>
2020-11-10 09:53:37 +0100

    menus
    added menus

---
+++ UI/ChooseLvlMenu.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_BackBtn_pressed():
+	get_tree().change_scene("res://UI/MainMenu.tscn")
+
+
+func _on_Lvl1Btn_pressed():
+	get_tree().change_scene("res://Levels/Level1.tscn")
---
+++ UI/ResolutionOptBtn.gd
@@ -0,0 +1,18 @@
+extends OptionButton
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	add_item("1920x1080")
+	add_item("1600x900")
+	add_item("1280x720")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass

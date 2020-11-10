be1de6c0dc869b91a302e0578b923872267134c5
BaseCrusher <ruzinski.jehuda@outlook.com>
2020-11-10 09:03:15 +0100

    added camera zoom

--- UI/LevelUI/LevelCamera.gd
+++ UI/LevelUI/LevelCamera.gd
@@ -11,6 +11,12 @@ onready var character : Node = level_global.get_character_node()
 var is_in_character = false
 var pressed = false
 
+var target_zoom = Vector2(1, 1)
+const zoom_amount = 0.15
+const zoom_speed = 5.0
+const min_zoom = Vector2(1, 1)
+var max_zoom = Vector2(10, 10)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	character.connect("mouse_entered", self, "_handle_mouse_entered_character")
@@ -25,6 +31,10 @@ func _handle_mouse_exited_character():
 	is_in_character = false
 	pass
 
+func _process(delta):
+	zoom = lerp(zoom, target_zoom, delta * zoom_speed)
+	pass
+
 func _input(event):
 	
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
@@ -36,4 +46,16 @@ func _input(event):
 
 	if pressed and event is InputEventMouseMotion:
 		global_position -= event.relative
+		pass
+		
+	if event is InputEventMouseButton and event.button_index == BUTTON_WHEEL_UP:
+		target_zoom -= Vector2(zoom_amount, zoom_amount)
+		if target_zoom < min_zoom:
+			target_zoom = min_zoom
+		pass
+	
+	if event is InputEventMouseButton and event.button_index == BUTTON_WHEEL_DOWN:
+		target_zoom += Vector2(zoom_amount, zoom_amount)
+		if target_zoom > max_zoom:
+			target_zoom = max_zoom
 		pass

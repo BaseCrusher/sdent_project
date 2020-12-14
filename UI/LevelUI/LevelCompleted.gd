5de866be4d9f4aeaeb7fba4737ccf5401395aa8c
AndreasRohr <andreas.rohr@students.fhnw.ch>
2020-12-14 11:55:28 +0100

    disable next lvl button when last level
    hardcoded for demo

--- UI/LevelUI/LevelCompleted.gd
+++ UI/LevelUI/LevelCompleted.gd
@@ -3,7 +3,11 @@ extends CanvasLayer
 
 func _on_level_completed():
 	$LevelCompleted.visible = true
-
+	
+	var lastlvl = get_tree().current_scene.name
+	if lastlvl == "Level4":
+		
+		$LevelCompleted/GridContainer/NextLvlBtn.visible = false
 
 
 func _on_NextLvlBtn_pressed():

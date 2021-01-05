1c50856b1d75f3257c214e22d6ee7da5915e218d
BaseCrusher <ruzinski.jehuda@outlook.com>
2021-01-05 09:46:58 +0100

    blackholes game over

--- GameObjects/BouncingPlate/BouncingPlate.gd
+++ GameObjects/BouncingPlate/BouncingPlate.gd
@@ -11,5 +11,14 @@ func _ready():
 	print(character.position)
 	print(position)
 
+func _physics_process(delta):
+	
+	pass
 
 
+
+func _on_Area2D_body_entered(body):
+	var rotation_delta = rotation
+	
+	
+	pass # Replace with function body.
